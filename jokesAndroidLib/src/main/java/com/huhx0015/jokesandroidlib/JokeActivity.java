8f70df7d780fa016a3ac6b34ba41c2b0cd6e1c1d
huhx0015 <mhuh@yoonhuh.com>
2016-04-06 21:27:45 -0700

    refactor: Removed action menu related code, as it is not being used.

--- jokesAndroidLib/src/main/java/com/huhx0015/jokesandroidlib/JokeActivity.java
+++ jokesAndroidLib/src/main/java/com/huhx0015/jokesandroidlib/JokeActivity.java
@@ -2,8 +2,6 @@ package com.huhx0015.jokesandroidlib;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /** -----------------------------------------------------------------------------------------------
  *  [JokeActivity] CLASS
@@ -19,30 +17,5 @@ public class JokeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
-    }
-
-    /** ACTIVITY LIFECYCLE EXTENSION METHODS ___________________________________________________ **/
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_joke, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
